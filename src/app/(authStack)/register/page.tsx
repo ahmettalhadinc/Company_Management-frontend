1e7375cc836d66c2a679798d4b21887ae881d62d
ahmettalhadinc <matbaahmet@gmail.com>
2025-06-12 17:57:14 +0300

    architect set up

---
+++ src/app/(authStack)/register/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>pafasfsage</div>
+  )
+}
+
+export default page
---
+++ src/app/(userStack)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>burası dashboard
+        
+    </div>
+  )
+}
+
+export default page
